b1588cd15db751122cf3b7183789ef09e6a4eb6a
Loupthevenin <loup.thevenin.1@gmail.com>
2025-08-03 20:21:22 +0200

    implement basic syscall tracing loop with ptrace and handle syscall stops

--- includes/ft_strace.h
+++ includes/ft_strace.h
@@ -1,12 +1,17 @@
 #ifndef FT_STRACE_H
 # define FT_STRACE_H
 
+# include <elf.h>
 # include <signal.h>
+# include <stdint.h>
 # include <stdio.h>
 # include <stdlib.h>
 # include <string.h>
 # include <sys/ptrace.h>
 # include <sys/types.h>
+# include <sys/uio.h>
+# include <sys/user.h>
+# include <sys/wait.h>
 # include <unistd.h>
 
 typedef struct s_args
--- srcs/tracer.c
+++ srcs/tracer.c
@@ -1,7 +1,62 @@
 #include "../includes/ft_strace.h"
 
+static void	loop_trace(pid_t child_pid, int *status)
+{
+	int						in_syscall;
+	struct user_regs_struct	regs;
+	struct iovec			iov;
+
+	in_syscall = 0;
+	while (1)
+	{
+		// Intercepte un syscall;
+		if (ptrace(PTRACE_SYSCALL, child_pid, NULL, NULL) == -1)
+			break ;
+		// Attend que ca s'arrete;
+		waitpid(child_pid, status, 0);
+		// execution terminé;
+		if (WIFEXITED(*status))
+			break ;
+		if (WIFSTOPPED(*status) && (WSTOPSIG(*status) & 0x80))
+		{
+			// Lecture registres;
+			iov.iov_base = &regs;
+			iov.iov_len = sizeof(regs);
+			if (ptrace(PTRACE_GETREGSET, child_pid, (void *)NT_PRSTATUS,
+					&iov) == -1)
+			{
+				perror("ptrace GETREGSET");
+				return ;
+			}
+			if (!in_syscall)
+			{
+				printf("%lld(", regs.orig_rax);
+				fflush(stdout);
+				in_syscall = 1;
+			}
+			else
+			{
+				printf(") = %lld", regs.rax);
+				in_syscall = 0;
+			}
+		}
+	}
+}
+
 void	tracer(pid_t child_pid, t_args *args)
 {
-	(void)child_pid;
+	int	status;
+
 	(void)args;
+	// Attendre le child
+	waitpid(child_pid, &status, 0);
+	if (WIFEXITED(status))
+		return ;
+	// Activer ptrace
+	if (ptrace(PTRACE_SETOPTIONS, child_pid, 0, PTRACE_O_TRACESYSGOOD) == -1)
+	{
+		perror("ptrace SETOPTIONS");
+		return ;
+	}
+	loop_trace(child_pid, &status);
 }
